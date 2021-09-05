41f644a2feb5cf220b5f5792a3553858063e0ba4
Tomaž Karer <tomaz@karer.si>
2021-09-05 07:18:19 +0200

    delo

--- keyboards/handwired/dactyl_manuform/tk/4x6/config.h
+++ keyboards/handwired/dactyl_manuform/tk/4x6/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // row-driven
 #define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { D4, C6, D7, B4, E6, B5 }
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
