c9868e3c35ec9efc8e6f0292598289f9e3ac98ee
Tomaž Karer <tomaz@karer.si>
2021-09-06 20:52:30 +0200

    deluje

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -45,10 +45,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
                           KC_LBRC, KC_RBRC,                                                KC_PLUS, KC_EQL,
-                                            RAISE,   KC_SPC,             KC_ENT,  LOWER,
-                                            KC_TAB,  KC_HOME,            KC_END,  KC_DEL,
-                                            KC_BSPC, KC_GRV,             KC_LGUI, KC_LALT
+                                            KC_1,   KC_2,                KC_1,  KC_2,
+                                            KC_3,   KC_4,                KC_3,  KC_4,
+                                            KC_5,   KC_6,                KC_5,  KC_6
     ),
+
+//    [_BASE] = LAYOUT(
+//        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+//        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+//        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+//                          KC_LBRC, KC_RBRC,                                                KC_PLUS, KC_EQL,
+//                                            RAISE,   KC_SPC,             KC_ENT,  LOWER,
+//                                            KC_TAB,  KC_HOME,            KC_END,  KC_DEL,
+//                                            KC_BSPC, KC_GRV,             KC_LGUI, KC_LALT
+//    ),
 
     [_LOWER] = LAYOUT(
         _______, _______, _______, _______, _______, KC_LBRC,             KC_RBRC, KC_P7,   KC_P8,   KC_P9,   RESET,   KC_PLUS,
