848b9852a4afadc470a81583b7ad09cbcd3aa6d2
Tomaž Karer <tomaz@karer.si>
2021-09-10 15:22:03 +0200

    delo

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ),
 
     [_LOWER] = LAYOUT(
-        _______, _______, _______, _______, _______, KC_LBRC,             KC_RBRC, KC_P7,   KC_P8,   KC_P9,   RESET,   KC_PLUS,
+        KC_GRAVE,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,   KC_8,   KC_9,   KC_0,   KC_3,
         _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END , KC_LPRN,             KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MINS, KC_PIPE,
         _______, _______, _______, _______, _______, _______,             _______, KC_P1,   KC_P2,   KC_P3,   KC_EQL,  KC_UNDS,
                           _______, KC_PSCR,                                                 _______, KC_P0,
@@ -79,6 +79,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______,             _______, _______,
                                             _______, _______,             _______, _______
     )
+//    [_LOWER] = LAYOUT(
+//        _______, _______, _______, _______, _______, KC_LBRC,             KC_RBRC, KC_P7,   KC_P8,   KC_P9,   RESET,   KC_PLUS,
+//        _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END , KC_LPRN,             KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MINS, KC_PIPE,
+//        _______, _______, _______, _______, _______, _______,             _______, KC_P1,   KC_P2,   KC_P3,   KC_EQL,  KC_UNDS,
+//                          _______, KC_PSCR,                                                 _______, KC_P0,
+//                                            _______, _______,             _______, _______,
+//                                            _______, _______,             _______, _______,
+//                                            _______, _______,             _______, _______
+//    ),
+//
+//    [_RAISE] = LAYOUT(
+//        _______, RESET,   _______, _______, _______, KC_LBRC,             KC_RBRC, _______, KC_NLCK, KC_INS,  KC_SLCK, KC_MUTE,
+//        _______, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_LPRN,             KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_VOLU,
+//        _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, KC_VOLD,
+//                          _______, _______,                                                 KC_EQL,  _______,
+//                                            _______, _______,             _______, _______,
+//                                            _______, _______,             _______, _______,
+//                                            _______, _______,             _______, _______
+//    )
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
