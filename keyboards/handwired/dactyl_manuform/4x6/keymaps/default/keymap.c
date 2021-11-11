424961bef326ae00478b0b8440e3c78cce28232e
Tomaz Karer <tomaz@karer.si>
2021-11-11 19:53:17 +0100

    mouse keys working

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -99,10 +99,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                           +-------------+ +-------------+
      */
     [_LOWER] = LAYOUT(
-        KC_GRAVE,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,   KC_8,   KC_9,   KC_0,    _______ ,
-        _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,               KC_F6,   KC_F7,  KC_F8,  KC_F9, KC_F10, _______,
-        _______, _______, _______, _______, _______, _______,             _______, _______,_______, KC_F11,KC_F12 , _______ ,
-                          _______, _______,                                                _______, _______,
+        KC_GRAVE,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,       KC_7,      KC_8,     KC_9,        KC_0,    _______ ,
+        _______, _______, KC_MS_BTN1, KC_MS_BTN3, KC_MS_BTN1, _______,       KC_MS_LEFT, KC_MS_DOWN,KC_MS_UP, KC_MS_RIGHT,_______ , _______ ,
+        _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                KC_F6,   KC_F7,  KC_F8,  KC_F9, KC_F10, _______,
+                          _______, _______,                                               KC_F11 , KC_F12,
                                             _______, _______,             _______, _______,
                                             _______, _______,             _______, _______,
                                             _______, _______,             _______, _______
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______,             _______, _______,
                                             _______, _______,             _______, _______
     )
-}
+};
 void persistent_default_layer_set(uint16_t default_layer) {
     eeconfig_update_default_layer(default_layer);
     default_layer_set(default_layer);
