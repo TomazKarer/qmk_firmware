6d835b0dc65cf8dc5adbe86a5e479f86da026394
Tomaz Karer <tomaz@karer.si>
2021-12-20 07:11:17 +0100

    adder run scripts for miryoku

---
+++ miryoku.sh
@@ -0,0 +1,1 @@
+qmk compile -c -kb handwired/dactyl_manuform/4x6 -km manna-harbour_miryoku
---
+++ right-miryoku.sh
@@ -0,0 +1,2 @@
+make clean handwired/dactyl_manuform/4x6:manna-harbour_miryoku:avrdude-split-right MIRYOKU_ALPHAS=QWERTY MIRYOKU_NAV=VI
+
